d4569bbc0ec60fcf79c37ddd5dc9bb96c2a7253c
SekoViper <damehlivingstone1@gmail.com>
2022-05-11 10:59:51 +0000

    Intial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+var toggler = document.querySelector('.toggler');
+var mainNav = document.querySelector('.main-nav');
+toggler.addEventListener('click', function(){
+    mainNav.classList.toggle('nav-active');
+    console.log('clicked')
+})
+
+
+function changeMode(){
+    var element = document.body;
+    element.classList.toggle('darkMode')
+    console.log('cliked')
+}
+
+
+// emolis project
+
+const stars = document.querySelectorAll('.fa-star');
+const emojis = document.querySelectorAll('.far');
+
+const colors = ["red", "lightgreen", "orange", "lightblue", "green"]
+
+updateRating(0)
+
+stars.forEach((star, index) => {
+    star.addEventListener('click', () => {
+        updateRating(index)
+    })
+})
+
+
+function updateRating(index){
+    stars.forEach((star, newIndex) => {
+        if(newIndex < index + 1){
+            star.classList.add('active')
+        } else{
+            star.classList.remove('active')
+        }
+    })
+
+    emojis.forEach(emoji =>{
+        emoji.style.transform = `translateX(-${index * 50}px)`;
+        emoji.style.color = colors[index]
+    });
+}
+
+
