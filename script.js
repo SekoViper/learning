7b621594acd78a4866645bf187be691d5fdf6e41
SekoViper <damehlivingstone1@gmail.com>
2022-05-19 08:50:42 +0000

    js added for the rotating image gallery

--- script.js
+++ script.js
@@ -60,3 +60,34 @@ counter.forEach((count) =>{
         }
     }
 })
+
+
+
+// rotating image project
+
+const imageContainerEl = document.querySelector('.imageContainer');
+const prevEl = document.getElementById('prev')
+const nextEl = document.getElementById('next')
+
+let x = 0
+let timer;
+prevEl.addEventListener("click", () => {
+    x = x + 45;
+    clearTimeout(timer);
+    UpdateGallery();
+})
+nextEl.addEventListener("click", () => {
+    x = x + 45;
+    clearTimeout(timer);
+    UpdateGallery();
+})
+
+function UpdateGallery(){
+    imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`
+    timer = setTimeout(() => {
+        x = x - 45;
+        UpdateGallery()
+    }, 3000)
+}
+
+UpdateGallery()
